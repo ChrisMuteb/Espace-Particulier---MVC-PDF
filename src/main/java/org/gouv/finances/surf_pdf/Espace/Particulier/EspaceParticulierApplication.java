0e817ed0b1314ae6bc5f854350ac9083b8cf0176
chriskayomb <ibond.chris@gmail.com>
2026-03-14 20:29:48 +0100

    Spring MVC project having thymeleaf, starter-webmvc & jasperreports

---
+++ src/main/java/org/gouv/finances/surf_pdf/Espace/Particulier/EspaceParticulierApplication.java
@@ -0,0 +1,13 @@
+package org.gouv.finances.surf_pdf.Espace.Particulier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspaceParticulierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspaceParticulierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gouv/finances/surf_pdf/Espace/Particulier/EspaceParticulierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gouv.finances.surf_pdf.Espace.Particulier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspaceParticulierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
